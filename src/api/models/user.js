bd7dd4edf634d0000147e720f1be92ee14eb3449
Thomas de Lachaux <thomas.delachaux@gmail.com>
2019-09-22 17:24:48 +0200

    user.nickname -> user.username

--- src/api/models/user.js
+++ src/api/models/user.js
@@ -2,7 +2,7 @@ module.exports = (sequelize, DataTypes) => {
   return sequelize.define('user', {
     id: { primaryKey: true, type: DataTypes.UUID, defaultValue: DataTypes.UUIDV4 },
     email: { type: DataTypes.STRING, validate: { isEmail: true }, unique: true, allowNull: false },
-    nickname: { type: DataTypes.STRING, unique: true, allowNull: false },
+    username: { type: DataTypes.STRING, unique: true, allowNull: false },
     firstname: { type: DataTypes.STRING, allowNull: false },
     lastname: { type: DataTypes.STRING, allowNull: false },
     password: { type: DataTypes.STRING, allowNull: false },
