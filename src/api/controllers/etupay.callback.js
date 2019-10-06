9916a7f735b95c2486291911cf80c13869ffedeb
Thomas de Lachaux <thomas.delachaux@gmail.com>
2019-10-06 19:43:09 +0200

    Pdf generation ok

--- src/api/controllers/etupay.callback.js
+++ src/api/controllers/etupay.callback.js
@@ -4,7 +4,7 @@ const etupay = require('@ung/node-etupay')({
   url: process.env.ARENA_ETUPAY_URL,
   key: process.env.ARENA_ETUPAY_KEY,
 });
-
+const generatePdf = require('../utils/sendPDF');
 const errorHandler = require('../utils/errorHandler');
 
 module.exports = (app) => {
@@ -13,11 +13,11 @@ module.exports = (app) => {
   app.get('/user/pay/return', etupay.middleware, async (req, res) => {
     // Jamais utilisée car géré par le middleware
 
-    const { Cart, CartItem, Item, Attribute } = req.app.locals.models;
+    const { Cart, CartItem, Item, Attribute, User, Team, Tournament } = req.app.locals.models;
 
     try {
       if (!req.query.payload) {
-        return res.redirect(`${process.env.ARENA_ETUPAY_ERRORURL}?error=NO_PAYLOAD`);
+        return res.redirect(`${process.env.ARENA_ETUPAY_ERRORURL}&error=NO_PAYLOAD`);
       }
 
       // Récupère le cartId depuis le payload envoyé à /carts/:id/pay
@@ -43,21 +43,37 @@ module.exports = (app) => {
       });
 
       if (!cart) {
-        return res.redirect(`${process.env.ARENA_ETUPAY_ERRORURL}?error=CART_NOT_FOUND`);
+        return res.redirect(`${process.env.ARENA_ETUPAY_ERRORURL}&error=CART_NOT_FOUND`);
       }
-
 
       cart.transactionState = req.etupay.step;
       cart.transactionId = req.etupay.transactionId;
 
       if (cart.transactionState !== 'paid') {
         await cart.save();
-        return res.redirect(`${process.env.ARENA_ETUPAY_ERRORURL}?error=TRANSACTION_ERROR`);
+        return res.redirect(`${process.env.ARENA_ETUPAY_ERRORURL}&error=TRANSACTION_ERROR`);
       }
 
       cart.paidAt = fn('NOW');
+      // todo: warning DEV
+      //await cart.save();
 
-      await cart.save();
+
+      let pdfTickets = await Promise.all(cart.cartItems.map(async (cartItem) => {
+        if (cartItem.item.key === 'player' || cartItem.item.key === 'visitor') {
+          // todo: moche à cause de seuquelize, peut etre moyen de raccourcir en une requête
+          const forUser = await User.findByPk(cartItem.forUserId);
+          console.log(cartItem.item.name);
+          return generatePdf(forUser);
+        }
+        return null;
+      }));
+
+      pdfTickets = pdfTickets.filter((ticket) => ticket !== null);
+
+
+      console.log(pdfTickets.length);
+
 
       /*
       tickets = map(ticket)
@@ -68,7 +84,13 @@ module.exports = (app) => {
       */
 
 
-      return res.redirect(process.env.ARENA_ETUPAY_SUCCESSURL);
+      // todo: DEV
+      return res
+        .status(200)
+        .json(pdfTickets.length)
+        .end();
+
+      // return res.redirect(process.env.ARENA_ETUPAY_SUCCESSURL);
     }
     catch (err) {
       return errorHandler(err, res);
