e4123bb79ee4e5abe3f6f1c40dad4af35438ec6c
Valentin <valentinpeltier@yahoo.fr>
2019-10-11 00:47:41 +0200

    fix GET /users/:userId/ticket

--- src/api/controllers/admin/forcepay.js
+++ src/api/controllers/admin/forcepay.js
@@ -3,7 +3,7 @@ const moment = require('moment');
 
 const hasPermission = require('../../middlewares/hasPermission');
 const isAuth = require('../../middlewares/isAuth');
-const sendPdf = require('../../utils/sendPDF');
+const generateTicket = require('../../utils/generateTicket');
 const errorHandler = require('../../utils/errorHandler');
 const validateBody = require('../../middlewares/validateBody');
 const log = require('../../utils/log')(module);
@@ -66,7 +66,7 @@ module.exports = (app) => {
       log.info(`Forced ${user.name}'s payment`);
 
       user = await User.findByPk(user.id, { include: [Team, Order] }); // add order to user
-      await sendPdf(user);
+      await generateTicket(user);
       log.info(`Mail sent to ${user.name}`);
 
       return res
