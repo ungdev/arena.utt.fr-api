9908ab317a44b0842e98a45b5574a28757ad89a5
nyc4m <baptiste.prunot@utt.fr>
2019-10-14 14:19:53 +0200

    remove auto-discovering controller

--- src/api/controllers/index.js
+++ src/api/controllers/index.js
@@ -1,31 +1,19 @@
 /* eslint-disable global-require, import/no-dynamic-require */
-const express = require('express');
+const Auth = require('./auth/auth.js')
+
+const Express = require('express');
 const fs = require('fs');
 const path = require('path');
 const log = require('../utils/log')(module);
 
-module.exports = (app) => {
-  const router = new express.Router();
+const mainRoutes = (models) => {
+  const mainRouter = Express.Router()
+  mainRouter.use('/auth', Auth(models))
+  return mainRouter
+}
 
-  router.locals = app.locals;
-
-  const traverseDir = (dir) => {
-    fs.readdirSync(dir)
-      .forEach((file) => {
-        const fullPath = path.join(dir, file);
-        if (fs.lstatSync(fullPath)
-          .isDirectory()) {
-          traverseDir(fullPath);
-        }
-        else if (!fullPath.endsWith('index.js') && fullPath.slice(-3) === '.js') {
-          const controller = require(fullPath.slice(0, -3));
-          controller(router);
-        }
-      });
-  };
-
-  traverseDir(__dirname);
-  log.info('Controllers imported');
-
-  return router;
+module.exports = (models) => {
+  const api = Express()
+  api.use('/api/v1', mainRoutes(models))
+  return api
 };
