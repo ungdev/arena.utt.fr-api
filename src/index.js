b7e721bfa042d6d3db294e2354b27a5010ecd425
nyc4m <baptiste.prunot@utt.fr>
2019-10-14 14:20:35 +0200

    remove express init

--- src/index.js
+++ src/index.js
@@ -3,25 +3,27 @@ const database = require('./database');
 const socket = require('./socket');
 const arena = require('./arena');
 const toornament = require('./toornament');
+const Express = require('express')
+const Api = require("./api/controllers")
 
-module.exports = async (app) => {
+module.exports = async () => {
   const { sequelize, models } = await database();
 
-  arena(app);
+  const api = Api(models)
 
-  const server = http.Server(app);
+  //const server = http.Server(app);
   const io = socket(http, sequelize, models);
 
-  app.locals.app = app;
-  app.locals.server = server;
-  app.locals.db = sequelize;
-  app.locals.models = models;
-  app.locals.io = io;
-  app.locals.toornament = toornament;
+ // app.locals.app = app;
+ // app.locals.server = server;
+ // app.locals.db = sequelize;
+ // app.locals.models = models;
+ // app.locals.io = io;
+ // app.locals.toornament = toornament;
 
   if (process.send) {
     process.send('ready');
   }
 
-  return app;
+  return api;
 };
