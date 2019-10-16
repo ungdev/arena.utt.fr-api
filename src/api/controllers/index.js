86254c68305775fafce0f3cb90ae26515b409218
nyc4m <baptiste.prunot@utt.fr>
2019-10-16 23:04:10 +0200

    move express init outside controllers

--- src/api/controllers/index.js
+++ src/api/controllers/index.js
@@ -1,20 +1,14 @@
 /* eslint-disable global-require, import/no-dynamic-require */
 
-const morgan = require('morgan');
-const helmet = require('helmet');
-const cors = require('cors');
-const bodyParser = require('body-parser');
 const Express = require('express');
-const error = require('../middlewares/error');
 const isAuth = require('../middlewares/isAuth.js');
-const log = require('../utils/log')(module);
 
 const Auth = require('./auth');
 const Cart = require('./cart');
 const User = require('./user');
 const Tournament = require('./tournament');
 
-const mainRoutes = models => {
+const MainRoutes = models => {
     const mainRouter = Express.Router();
     mainRouter.use('/auth', Auth(models));
     mainRouter.use('/users', User(models));
@@ -23,23 +17,4 @@ const mainRoutes = models => {
     return mainRouter;
 };
 
-module.exports = models => {
-    const api = Express();
-    api.use(
-        morgan(process.env.NODE_ENV === 'development' ? 'dev' : 'combined', {
-            stream: log.stream,
-        })
-    );
-
-    api.use(helmet());
-    api.use(cors());
-    api.use(bodyParser.json());
-
-    api.use('/api/v1', mainRoutes(models));
-
-    api.use(error.converter);
-    api.use(error.notFound);
-    api.use(error.handler);
-
-    return api;
-};
+module.exports = MainRoutes;
--- src/index.js
+++ src/index.js
@@ -2,16 +2,23 @@ const http = require('http');
 const Express = require('express');
 const database = require('./database');
 const socket = require('./socket');
-const toornament = require('./toornament');
-const Api = require('./api/controllers');
+const MainRoutes = require('./api/controllers');
+const helmet = require('helmet');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const error = require('./api/middlewares/error.js');
+const morgan = require('morgan');
+const log = require('./api/utils/log.js');
 
 module.exports = async () => {
     const { sequelize, models } = await database();
 
-    const api = Api(models);
-
-    // const server = http.Server(app);
-    const io = socket(http, sequelize, models);
+    const api = Express();
+    api.use(
+        morgan(process.env.NODE_ENV === 'development' ? 'dev' : 'combined', {
+            stream: log.stream,
+        })
+    );
 
     // app.locals.app = app;
     // app.locals.server = server;
@@ -19,6 +26,21 @@ module.exports = async () => {
     api.locals.models = models;
     // app.locals.io = io;
     // app.locals.toornament = toornament;
+
+    api.use(helmet());
+    api.use(cors());
+    api.use(bodyParser.json());
+
+    api.use('/api/v1', MainRoutes(models));
+
+    api.use(error.converter);
+    api.use(error.notFound);
+    api.use(error.handler);
+
+    return api;
+
+    // const server = http.Server(app);
+    const io = socket(http, sequelize, models);
 
     if (process.send) {
         process.send('ready');
