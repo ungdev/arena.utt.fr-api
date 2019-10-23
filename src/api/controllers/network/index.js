358628a62f8efed91bfcbc33cd1e885e058b864c
nyc4m <baptiste.prunot@utt.fr>
2019-10-23 14:16:05 +0200

    refactore get all networks

--- src/api/controllers/network/index.js
+++ src/api/controllers/network/index.js
@@ -5,6 +5,7 @@ const List = require('./get.all.js');
 const Network = models => {
     const router = Express.Router();
     router.post('/', CheckCreate, Create(models.Network));
+    router.get('/', List(models.Network, models.User, models.Team));
     return router;
 };
 module.exports = Network;
