9b6b87f2fff9b277603879e5f4b536214fdda20e
nyc4m <baptiste.prunot@utt.fr>
2019-10-24 14:52:31 +0200

    refactore get on network by mac

--- src/api/controllers/network/index.js
+++ src/api/controllers/network/index.js
@@ -1,11 +1,18 @@
 const Express = require('express');
 const { Create, CheckCreate } = require('./create.js');
 const List = require('./get.all.js');
+const GetByMac = require('./get.js');
+
+const mac = 'mac';
 
 const Network = models => {
     const router = Express.Router();
     router.post('/', CheckCreate, Create(models.Network));
     router.get('/', List(models.Network, models.User, models.Team));
+    router.get(
+        `/:${mac}`,
+        GetByMac(mac, models.Network, models.User, models.Team)
+    );
     return router;
 };
 module.exports = Network;
