5c8055afa950bc4cad2358534232fa9ce212911a
nyc4m <baptiste.prunot@utt.fr>
2019-10-26 13:41:45 +0200

    refactore edit an item in cart

--- src/api/controllers/cart/index.js
+++ src/api/controllers/cart/index.js
@@ -4,14 +4,21 @@ const isAuth = require('../../middlewares/isAuth.js');
 
 const CreateCart = require('./create.js');
 const { AddItemToCart, CheckAddItem } = require('./add-item.js');
+const { Edit, CheckEdit } = require('./edit.js');
 
 const cartId = 'cartId';
+const itemId = 'itemId';
   const router = Express.Router();
   router.post('/', CreateCart(models.Cart));
     router.post(
         `/:${cartId}/add`,
         [CheckAddItem],
         AddItemToCart(cartId, models.CartItem, models.User, models.Cart)
+    );
+    router.put(
+        `/:${cartId}/cartItems/:${itemId}`,
+        CheckEdit,
+        Edit(cartId, itemId, models.CartItem, models.User)
     );
   return router;
 };
