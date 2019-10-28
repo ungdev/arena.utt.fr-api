b58f8f52e7fd9ac5ee7f70526e36a82ce71025a4
nyc4m <baptiste.prunot@utt.fr>
2019-10-28 10:26:47 +0100

    add online check for etupay

--- src/api/controllers/cart/index.js
+++ src/api/controllers/cart/index.js
@@ -7,41 +7,42 @@ const DeleteItemFromCart = require('./delete.js');
 const { AddItemToCart, CheckAddItem } = require('./add-item.js');
 const { Edit, CheckEdit } = require('./edit.js');
 const GetItemFromCart = require('./get-item-from-cart.js');
-const SuccessfulPayment = require('./etupay.callback');
+const { SuccessfulPayment, EtupayAvailable } = require('./etupay.callback');
 
 const cartId = 'cartId';
 const itemId = 'itemId';
 
 const Cart = models => {
-    const router = Express.Router();
-    router.post('/', CreateCart(models.Cart));
-    router.delete(
-        `/:${cartId}/cartItems/:${itemId}`,
-        DeleteItemFromCart(cartId, itemId, models.CartItem)
-    );
-    router.post(
-        `/:${cartId}/add`,
-        [CheckAddItem],
-        AddItemToCart(cartId, models.CartItem, models.User, models.Cart)
-    );
-    router.put(
-        `/:${cartId}/cartItems/:${itemId}`,
-        CheckEdit,
-        Edit(cartId, itemId, models.CartItem, models.User)
-    );
+  const router = Express.Router();
+  router.post('/', CreateCart(models.Cart));
+  router.delete(
+    `/:${cartId}/cartItems/:${itemId}`,
+    DeleteItemFromCart(cartId, itemId, models.CartItem)
+  );
+  router.post(
+    `/:${cartId}/add`,
+    [CheckAddItem],
+    AddItemToCart(cartId, models.CartItem, models.User, models.Cart)
+  );
+  router.put(
+    `/:${cartId}/cartItems/:${itemId}`,
+    CheckEdit,
+    Edit(cartId, itemId, models.CartItem, models.User)
+  );
 
-    router.get(
-        `/:${cartId}/cartItems/:${itemId}`,
-        GetItemFromCart(
-            cartId,
-            itemId,
-            models.CartItem,
-            models.Item,
-            models.Attribute
-        )
-    );
-    router.get('/return', etupay.middleware, SuccessfulPayment());
-    return router;
+  router.get(
+    `/:${cartId}/cartItems/:${itemId}`,
+    GetItemFromCart(
+      cartId,
+      itemId,
+      models.CartItem,
+      models.Item,
+      models.Attribute
+    )
+  );
+  router.get('/return', etupay.middleware, SuccessfulPayment());
+  router.get('/callback', EtupayAvailable());
+  return router;
 };
 
 module.exports = Cart;
