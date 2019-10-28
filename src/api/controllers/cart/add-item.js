13529514544026178a53aedd4390b0783bb769c7
nyc4m <baptiste.prunot@utt.fr>
2019-10-28 10:42:31 +0100

    change indent

--- src/api/controllers/cart/add-item.js
+++ src/api/controllers/cart/add-item.js
@@ -4,15 +4,15 @@ const errorHandler = require('../../utils/errorHandler');
 const validateBody = require('../../middlewares/validateBody');
 
 const CheckAddItem = [
-    check('itemId').isInt(),
-    check('quantity').isInt(),
-    check('attributeId')
-        .optional()
-        .isInt(),
-    check('forUserId')
-        .optional()
-        .isUUID(),
-    validateBody(),
+  check('itemId').isInt(),
+  check('quantity').isInt(),
+  check('attributeId')
+    .optional()
+    .isInt(),
+  check('forUserId')
+    .optional()
+    .isUUID(),
+  validateBody(),
 ];
 
 /**
@@ -31,52 +31,52 @@ const CheckAddItem = [
  * }
  */
 const AddItemToCart = (cartIdString, cartItemModel, userModel, cartModel) => {
-    return async (req, res) => {
-        const forUser = req.body.forUserId || req.user.id;
-        const cartId = req.params[cartIdString];
-        try {
-            const user = await userModel.findByPk(forUser);
-            if (!user) {
-                return res
-                    .status(404)
-                    .json({ error: 'USER_NOT_FOUND' })
-                    .end();
-            }
-            // A modifier après pour l'admin
-            const cartCount = await cartModel.count({
-                where: {
-                    id: req.params.cartId,
-                    userId: req.user.id,
-                    transactionState: 'draft',
-                },
-            });
+  return async (req, res) => {
+    const forUser = req.body.forUserId || req.user.id;
+    const cartId = req.params[cartIdString];
+    try {
+      const user = await userModel.findByPk(forUser);
+      if (!user) {
+        return res
+          .status(404)
+          .json({ error: 'USER_NOT_FOUND' })
+          .end();
+      }
+      // A modifier après pour l'admin
+      const cartCount = await cartModel.count({
+        where: {
+          id: req.params.cartId,
+          userId: req.user.id,
+          transactionState: 'draft',
+        },
+      });
 
-            if (cartCount !== 1) {
-                return res
-                    .status(400)
-                    .json({ error: 'BAD_REQUEST' })
-                    .end();
-            }
+      if (cartCount !== 1) {
+        return res
+          .status(400)
+          .json({ error: 'BAD_REQUEST' })
+          .end();
+      }
 
-            const cartItem = {
-                ...req.body,
-                userId: req.user.id, // Attention ! Pas compatible avec admin
-                forUserId: forUser,
-                cartId: cartId,
-            };
+      const cartItem = {
+        ...req.body,
+        userId: req.user.id, // Attention ! Pas compatible avec admin
+        forUserId: forUser,
+        cartId: cartId,
+      };
 
-            // Attention: pas de verification d'attribute si ça peut correspondre à un itemId
-            // Est-ce utile ?
-            const newCartItem = await cartItemModel.create(cartItem);
+      // Attention: pas de verification d'attribute si ça peut correspondre à un itemId
+      // Est-ce utile ?
+      const newCartItem = await cartItemModel.create(cartItem);
 
-            return res
-                .status(200)
-                .json(newCartItem)
-                .end();
-        } catch (err) {
-            return errorHandler(err, res);
-        }
-    };
+      return res
+        .status(200)
+        .json(newCartItem)
+        .end();
+    } catch (err) {
+      return errorHandler(err, res);
+    }
+  };
 };
 
 module.exports = { AddItemToCart, CheckAddItem };
