54835e9d4694a1afc8f7de90a6bdb7578d1ce58f
ValentinPeltier <valentinpeltier@yahoo.fr>
2019-11-15 23:59:44 +0100

    fix etupay return route

--- src/api/controllers/cart/index.js
+++ src/api/controllers/cart/index.js
@@ -1,12 +1,13 @@
 const Express = require('express');
 
-const etupay = require('../../utils/etupay.js');
+const etupay = require('../../utils/etupay');
+const isAuth = require('../../middlewares/isAuth');
 
-const CreateCart = require('./create.js');
-const DeleteItemFromCart = require('./delete.js');
-const { AddItemToCart, CheckAddItem } = require('./add-item.js');
-const { Edit, CheckEdit } = require('./edit.js');
-const GetItemFromCart = require('./get-item-from-cart.js');
+const CreateCart = require('./create');
+const DeleteItemFromCart = require('./delete');
+const { AddItemToCart, CheckAddItem } = require('./add-item');
+const { Edit, CheckEdit } = require('./edit');
+const GetItemFromCart = require('./get-item-from-cart');
 const { SuccessfulPayment, EtupayAvailable } = require('./etupay.callback');
 
 const cartId = 'cartId';
@@ -14,24 +15,28 @@ const itemId = 'itemId';
 
 const Cart = (models) => {
   const router = Express.Router();
-  router.post('/', CreateCart(models.Cart));
+  router.post('/', isAuth(), CreateCart(models.Cart));
   router.delete(
     `/:${cartId}/cartItems/:${itemId}`,
+    isAuth(),
     DeleteItemFromCart(cartId, itemId, models.CartItem),
   );
   router.post(
     `/:${cartId}/add`,
-    [CheckAddItem],
+    isAuth(),
+    CheckAddItem,
     AddItemToCart(cartId, models.CartItem, models.User, models.Cart),
   );
   router.put(
     `/:${cartId}/cartItems/:${itemId}`,
+    isAuth(),
     CheckEdit,
     Edit(cartId, itemId, models.CartItem, models.User),
   );
 
   router.get(
     `/:${cartId}/cartItems/:${itemId}`,
+    isAuth(),
     GetItemFromCart(
       cartId,
       itemId,
@@ -40,7 +45,16 @@ const Cart = (models) => {
       models.Attribute,
     ),
   );
-  router.get('/return', etupay.middleware, SuccessfulPayment());
+  router.get('/return',
+    etupay.middleware,
+    SuccessfulPayment(
+      models.Cart,
+      models.CartItem,
+      models.Item,
+      models.Attribute,
+      models.User,
+    ),
+  );
   router.post('/callback', EtupayAvailable());
   return router;
 };
--- src/api/controllers/index.js
+++ src/api/controllers/index.js
@@ -1,6 +1,6 @@
 const Express = require('express');
-const isAuth = require('../middlewares/isAuth.js');
-const resttrictToIp = require('../middlewares/restrictToIP.js');
+const isAuth = require('../middlewares/isAuth');
+const resttrictToIp = require('../middlewares/restrictToIP');
 const hasPermission = require('../middlewares/hasPermission');
 
 const Auth = require('./auth');
@@ -18,13 +18,13 @@ const File = require('./files');
 const MainRoutes = models => {
   const mainRouter = Express.Router();
   mainRouter.use('/auth', Auth(models));
-  mainRouter.use('/users', [isAuth()], User(models));
-  mainRouter.use('/tournaments', [isAuth()], Tournament(models));
-  mainRouter.use('/carts', [isAuth()], Cart(models));
-  mainRouter.use('/teams', [isAuth()], Team(models));
-  mainRouter.use('/items', [isAuth()], Item(models));
-  mainRouter.use('/entry', [isAuth(), hasPermission('entry')], Entry(models));
-  mainRouter.use('/infos', [isAuth()], Info(models));
+  mainRouter.use('/users', isAuth(), User(models));
+  mainRouter.use('/tournaments', isAuth(), Tournament(models));
+  mainRouter.use('/carts', Cart(models));
+  mainRouter.use('/teams', isAuth(), Team(models));
+  mainRouter.use('/items', isAuth(), Item(models));
+  mainRouter.use('/entry', isAuth(), hasPermission('entry'), Entry(models));
+  mainRouter.use('/infos', isAuth(), Info(models));
   mainRouter.use('/files', File());
   mainRouter.use(
     '/network',
