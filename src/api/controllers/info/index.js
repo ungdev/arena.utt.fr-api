fe0ffaf4d0b495d8265730e1850c2949513f62a3
nyc4m <baptiste.prunot@utt.fr>
2019-11-28 20:46:39 +0100

    add SendNotificationToTournament

--- src/api/controllers/info/index.js
+++ src/api/controllers/info/index.js
@@ -4,22 +4,23 @@ const hasPermission = require('../../middlewares/hasPermission');
 const List = require('./list.js');
 const { Create, CheckCreate } = require('./create.js');
 const Delete = require('./delete.js');
+const SendNotificationToTournament = require('../../utils/notification-sender.js');
 
 const infoId = 'infoId';
 
-const Info = (models) => {
+const Info = models => {
   const router = Express.Router();
   router.get('/', List(models.Info, models.User));
   router.post(
     '/',
     hasPermission('anim'),
     CheckCreate,
-    Create(models.Info),
+    Create(models.Info, SendNotificationToTournament)
   );
   router.delete(
     `/:${infoId}`,
     hasPermission('anim'),
-    Delete(infoId, models.Info),
+    Delete(infoId, models.Info)
   );
   return router;
 };
